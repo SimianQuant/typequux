2c429bf070937b82c8bc9baca3de43395d814153
Harshad Deo <subterranean.hominid@gmail.com>
2018-10-02 11:54:02 +0530

    update copyright notice

--- typequux/src/main/scala/typequux/constraint/ToListConstraint.scala
+++ typequux/src/main/scala/typequux/constraint/ToListConstraint.scala
@@ -1,5 +1,5 @@
 /**
-  * Copyright 2017 Harshad Deo
+  * Copyright 2018 Harshad Deo
   *
   * Licensed under the Apache License, Version 2.0 (the "License");
   * you may not use this file except in compliance with the License.
