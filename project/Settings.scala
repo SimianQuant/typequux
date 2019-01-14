3520f0793b1900f431878e74c809e506e6980ac8
Harshad Deo <subterranean.hominid@gmail.com>
2019-01-14 13:48:20 +0530

    bump version to 0.8.0-SNAPSHOT

--- project/Settings.scala
+++ project/Settings.scala
@@ -1,6 +1,6 @@
 object Settings {
   object versions {
-    val project: String = "0.7.3-SNAPSHOT"
+    val project: String = "0.8.0-SNAPSHOT"
     val scala: String = "2.12.8"
     val scalaTest: String = "3.0.5"
   }
